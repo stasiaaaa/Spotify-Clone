f9b91f55259629e91f741723874597fab3e73817
Stasia <anastasia.saprunova@gmail.com>
2024-02-04 14:02:20 +0100

    Finished layout for Sidebar and Library component.

---
+++ spotify-clone/app/(site)/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="
+    bg-neutral-900
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overlow-y-auto">
+      
+    </div>
+  );
+}
